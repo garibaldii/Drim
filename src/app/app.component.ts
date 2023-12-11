4ee0583770d8c5310b0e804e1008d8b28ff29672
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2023-12-11 07:04:11 -0300

    dev interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TelaInicialComponent } from "./components/tela-inicial/tela-inicial.component";
+import { TelaCadastroComponent } from './components/tela-cadastro/tela-cadastro.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, TelaInicialComponent]
+    imports: [CommonModule, RouterOutlet, TelaInicialComponent, TelaCadastroComponent]
 })
 export class AppComponent {
   title = 'Drim';
---
+++ src/app/components/tela-cadastro/tela-cadastro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-cadastro',
+  standalone: true,
+  imports: [],
+  templateUrl: './tela-cadastro.component.html',
+  styleUrl: './tela-cadastro.component.css'
+})
+export class TelaCadastroComponent {
+
+}
